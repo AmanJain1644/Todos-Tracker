0ed09879603f77992ff13b11f575df49a3107e19
amanjain1644 <amanjain1644@gmail.com>
2024-07-21 22:56:06 +0530

    project added

---
+++ app.js
@@ -0,0 +1,159 @@
+function saveTodosToLocalStorage(todos){
+    localStorage.setItem('todos',JSON.stringify(todos));
+}
+
+function getTodosFromLocalStorage(){
+   return JSON.parse( localStorage.getItem('todos')) || [];
+}  
+
+function saveTodo(event){
+    const task = event.target.parentElement;
+    console.log(task)
+    const editedTitle = task.querySelector('.edit-title');
+    const editedDes = task.querySelector('.edit-description');
+    console.log(editedDes,editedTitle)
+    editedTitle.classList.add('editable');
+
+    editedDes.classList.add('editable');
+
+    const orignalTitle = task.querySelector('.task-title');
+    const orignalDes = task.querySelector('.task-description');
+
+    orignalTitle.classList.remove('editable');
+    orignalDes.classList.remove('editable');
+
+    orignalTitle.textContent = editedTitle.value;
+    orignalDes.textContent = editedDes.value;
+
+    event.target.innerHTML = 'edit';
+    event.target.onclick = editTodo;
+
+    updateLocalStorage();
+}
+
+function trackTodo(event){
+    const task = event.target.parentElement;
+    const tracker = task.querySelector('.tracker');
+    tracker.classList.remove('editable');
+    updateLocalStorage();
+}
+
+function editTodo(event){
+    const task = event.target.parentElement;
+    console.log(task)
+
+    const oldTitle = task.querySelector('.task-title');
+    const oldDes = task.querySelector('.task-description');
+
+    oldTitle.classList.add('editable');
+    oldDes.classList.add('editable');
+
+    const newTitle = task.querySelector('.edit-title');
+    const newDes = task.querySelector('.edit-description');
+
+    newTitle.classList.remove('editable');
+    newDes.classList.remove('editable');
+
+    event.target.innerHTML='save';
+    event.target.onclick =saveTodo;
+    updateLocalStorage();
+
+}
+
+function removeTodo(event){
+    const parent = event.target.parentNode;
+    parent.remove();
+    updateLocalStorage();
+}
+
+function addTodo(){
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
+
+    if(!title || !description){
+        alert('please give title and description');
+        return;
+    }
+    createTaskElement(title,description);
+    document.getElementById('title').value = '';
+    document.getElementById('description').value = '';
+    updateLocalStorage();
+}
+
+function createTaskElement(title,description,completed){
+    const taskContainer = document.getElementById('allTasks');
+    const task = document.createElement('div');
+    task.setAttribute('class','task');
+
+    const titleContainer = document.createElement('p');
+    titleContainer.innerHTML =`${title}`;
+    titleContainer.setAttribute('class','task-title');
+
+    const editTitleInput = document.createElement('input');
+    editTitleInput.setAttribute('class', 'edit-title editable');
+    editTitleInput.value = title;
+
+    const desContainer = document.createElement('div');
+    desContainer.innerHTML = `${description}`;
+    desContainer.setAttribute('class','task-description');
+
+    const editDescriptionInput = document.createElement('textarea');
+    editDescriptionInput.setAttribute('class','editable edit-description description');
+    editDescriptionInput.value = description;
+
+
+    const donebtn = document.createElement('button');
+    donebtn.innerHTML='done';
+    donebtn.setAttribute('class','donebtn');
+    donebtn.onclick = trackTodo;
+    
+    const editbtn = document.createElement('button');        
+    editbtn.innerHTML='edit';
+    editbtn.setAttribute('class','editbtn');
+    editbtn.onclick = editTodo;
+
+    const tracker = document.createElement('div');
+    tracker.setAttribute('class','tracker editable');
+    tracker.innerHTML='Completed';
+
+    const delbtn = document.createElement('button');
+    delbtn.innerHTML='del';
+    delbtn.setAttribute('class','delbtn');
+    delbtn.onclick = removeTodo;
+
+    task.appendChild(tracker);       
+    task.appendChild(titleContainer);
+    task.appendChild(desContainer);            
+    task.appendChild(editTitleInput);
+    task.appendChild(editDescriptionInput);
+    task.appendChild(editbtn);
+    task.appendChild(donebtn);  
+    task.appendChild(delbtn);
+    taskContainer.appendChild(task);
+    if(completed){
+        tracker.classList.remove('editable');
+    }
+}    
+
+function loadTodos(){
+    const tasks = getTodosFromLocalStorage();
+    tasks.forEach(task => {
+        createTaskElement(task.title,task.description,task.completed);               
+    });
+}
+
+function updateLocalStorage(){
+    const tasks=[];
+    document.querySelectorAll('.task').forEach(task=>{
+        const title = task.querySelector('.task-title').textContent;
+        const description = task.querySelector('.task-description').textContent;
+        const completed = !task.querySelector('.tracker').classList.contains('editable');
+        tasks.push({title, description,completed});
+    });
+    saveTodosToLocalStorage(tasks);
+}
+
+const addbtn = document.getElementById('addbtn');
+addbtn.onclick=addTodo;
+window.addEventListener('load', loadTodos);
+
